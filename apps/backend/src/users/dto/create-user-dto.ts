55db53755abde714d2d3a10689c1e4330ca0a443
NZIMA Ivan <nzimaivan007@gmail.com>
2024-05-27 15:13:58 +0100

    [back > sign up feat] document endpoint

--- apps/backend/src/users/dto/create-user-dto.ts
+++ apps/backend/src/users/dto/create-user-dto.ts
@@ -1,12 +1,16 @@
 import { IsNotEmpty, IsNumber } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({ example: 'john_doe', description: 'Username of the user' })
   @IsNotEmpty()
   username: string;
 
+  @ApiProperty({ example: 'password123', description: 'Password of the user' })
   @IsNotEmpty()
   password: string;
 
+  @ApiProperty({ example: 123456789, description: 'Phone number of the user' })
   @IsNotEmpty()
   @IsNumber()
   phone: number;
