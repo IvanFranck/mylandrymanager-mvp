98fe6c36b014ffafe9475a88a0ef97e7ccee6767
NZIMA Ivan <nzimaivan007@gmail.com>
2024-05-27 16:53:55 +0100

    [front > sign up ui] create géneric form and use it on login view

--- apps/frontend/src/lib/api/login.ts
+++ apps/frontend/src/lib/api/login.ts
@@ -3,7 +3,7 @@ import { axiosInstance } from "../plugins/axios"
 
 export const LoginFormSchema = z.object({
     password: z.string(),
-    phone: z.string().min(9).max(9, 'invalid phone number')
+    phone: z.string().min(9, 'Le numéro de téléphone doit contenir 9 chiffres').max(9, 'Le numéro de téléphone doit contenir 9 chiffres')
 })
 export const loginQuery = async (data: z.infer<typeof LoginFormSchema>) => {
     return await axiosInstance.post('/auth/login', { password: data.password, phone: +data.phone} )
