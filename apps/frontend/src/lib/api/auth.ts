e52bac101d7a7e664c2166ad4617951c8e0f6d2d
NZIMA Ivan <nzimaivan007@gmail.com>
2024-05-27 18:11:39 +0100

    [front > sign up ui] creating sign up page and hooking it up to API

---
+++ apps/frontend/src/lib/api/auth.ts
@@ -0,0 +1,25 @@
+import z from "zod"
+import { axiosInstance } from "../plugins/axios"
+
+
+
+// login
+export const LoginFormSchema = z.object({
+    password: z.string(),
+    phone: z.string().min(9, 'Le numéro de téléphone doit contenir 9 chiffres').max(9, 'Le numéro de téléphone doit contenir 9 chiffres')
+})
+
+export const loginQuery = async (data: z.infer<typeof LoginFormSchema>) => {
+    return await axiosInstance.post('/auth/login', { password: data.password, phone: +data.phone} )
+}
+
+// register 
+export const RegisterFormSchema = z.object({
+    username: z.string(),
+    password: z.string(),
+    phone: z.string().min(9, 'Le numéro de téléphone doit contenir 9 chiffres').max(9, 'Le numéro de téléphone doit contenir 9 chiffres')
+})
+
+export const registerQuery = async (data: z.infer<typeof RegisterFormSchema>) => {
+    return await axiosInstance.post('/users/signup', { username: data.username, password: data.password, phone: +data.phone })  
+}
--- apps/frontend/src/views/login-view.tsx
+++ apps/frontend/src/views/login-view.tsx
@@ -1,5 +1,5 @@
 import { z } from "zod"
-import { LoginFormSchema, loginQuery } from "@/lib/api/login"
+import { LoginFormSchema, loginQuery } from "@/lib/api/auth"
 import { useMutation } from "@tanstack/react-query"
 import { AxiosError, AxiosResponse } from "axios"
 import { useToast } from "@/components/ui/use-toast"
@@ -9,12 +9,13 @@ import { AXIOS_ACCESS_TOKEN, AXIOS_REFRESH_TOKEN, STORED_USER_DATA } from "@/com
 import { GenericForm } from "@/components/ui/generic-form"
 import { Button } from "@/components/ui/button"
 import { Loader } from "lucide-react"
+import { LOGIN_QUERY_KEY } from "@/common/constants/query-keys"
 
 
 export default function LoginView() {
     const { toast } = useToast()
     const navigate = useNavigate()
-    const queryKey = ['loginKey']
+    const queryKey = LOGIN_QUERY_KEY
     const { mutateAsync, isPending } = useMutation({
         mutationKey: queryKey,
         mutationFn: async (data: z.infer<typeof LoginFormSchema>) => {
@@ -32,6 +33,10 @@ export default function LoginView() {
             localStorage.setItem(AXIOS_ACCESS_TOKEN, authDetails.accessToken)
             localStorage.setItem(AXIOS_REFRESH_TOKEN, authDetails.refreshToken)
             localStorage.setItem(STORED_USER_DATA, JSON.stringify(authDetails.user))
+            toast({
+                variant: 'success',
+                description: 'Vous êtes connecté!'
+            })
             navigate('/commands')
         }
     })
@@ -40,7 +45,7 @@ export default function LoginView() {
     }
     return (
         <div className="w-full min-h-screen flex flex-col justify-center items-center bg-black text-white">
-            <div className="flex flex-col px-3">
+            <div className="flex flex-col w-full px-5">
                 <h1 className="text-4xl font-medium">Connectez vous à votre compte.</h1>
                 <div className="mt-8">
                     <GenericForm
@@ -63,7 +68,7 @@ export default function LoginView() {
 
 const SubmitButton = ({isPending}: {isPending: boolean}) => {
     return (
-        <Button disabled={isPending} className="bg-white text-black rounded-full text-lg py-5" type="submit">
+        <Button disabled={isPending} className="bg-white text-black rounded-lg text-lg py-5" type="submit">
             Se connecter
             {isPending && <Loader size={18} className="animate-spin ml-3" />}
         </Button>
