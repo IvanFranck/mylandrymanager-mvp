d9f0004b06224059fc4b61ceee3a4b3f694cf2d4
NZIMA Ivan <nzimaivan007@gmail.com>
2024-06-17 17:05:19 +0100

    setting up tremor/reatc

--- apps/frontend/src/components/app/commands/CommandsStatusFilter.tsx
+++ apps/frontend/src/components/app/commands/CommandsStatusFilter.tsx
@@ -11,10 +11,10 @@ export const CommandsStatusFilter = ({status, setStatus}: CommandsStatusFilterPr
     return (
         <Tabs defaultValue={status ?? 'all'}className="w-full">
             <TabsList className="w-full grid grid-cols-4">
-                <TabsTrigger onClick={()=>setStatus(undefined)} value="all">Toute</TabsTrigger>
-                <TabsTrigger onClick={()=>setStatus('PAID')} value="PAID">Réglée</TabsTrigger>
+                <TabsTrigger onClick={()=>setStatus(undefined)} value="all">Toutes</TabsTrigger>
+                <TabsTrigger onClick={()=>setStatus('PAID')} value="PAID">Réglées</TabsTrigger>
                 <TabsTrigger onClick={()=>setStatus('PENDING')} value="PENDING">En cours</TabsTrigger>
-                <TabsTrigger onClick={()=>setStatus('NOT_PAID')} value="NOT_PAID">Non réglée</TabsTrigger>
+                <TabsTrigger onClick={()=>setStatus('NOT_PAID')} value="NOT_PAID">Non réglées</TabsTrigger>
             </TabsList>
         </Tabs>
     )
--- apps/frontend/src/views/home-view.tsx
+++ apps/frontend/src/views/home-view.tsx
@@ -1,3 +1,15 @@
+import {Card} from "@tremor/react"
 export function HomeView() {
-    return (<div>This is the home view</div>)
+    return (
+        <div className="px-5">
+            <Card className="mx-auto max-w-md">      
+                <h4 className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">        
+                    Sales      
+                </h4>      
+                <p className="text-tremor-metric font-semibold text-tremor-content-strong dark:text-dark-tremor-content-strong">        
+                    $71,465      
+                </p>    
+            </Card>
+        </div>
+    )
 }
