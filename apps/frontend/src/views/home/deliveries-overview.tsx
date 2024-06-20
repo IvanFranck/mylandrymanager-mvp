9e4df6579f367c5974017a1c88ac02d917bdf66d
NZIMA Ivan <nzimaivan007@gmail.com>
2024-06-20 17:23:57 +0100

    get the list of delivrery days and span it on a calendar

--- apps/frontend/src/views/home/deliveries-overview.tsx
+++ apps/frontend/src/views/home/deliveries-overview.tsx
@@ -1,12 +1,12 @@
 import { Calendar } from "@/components/ui/calendar"
-import { DayContentProps } from "react-day-picker"
-import { endOfMonth, startOfMonth } from "date-fns"
-import React from "react"
+import { endOfMonth, isSameDay, startOfMonth } from "date-fns"
+import React, { useEffect, useState } from "react"
 import { fr } from "date-fns/locale"
-import CalendarCustomDayContent from "@/components/ui/calendar-custom-day-content"
 import { useGetAllCommands } from "@/lib/hooks/use-cases/commands/useGetAllCommands"
-
-
+import { DayMouseEventHandler, Matcher } from "react-day-picker"
+import './delivery-date.css'
+import { CommandDeliveryListItem } from "@/components/app/commands/command-delivery-list-item"
+import { CommandsEntity } from "@/lib/types/entities"
 
 export default function DeliveriesOverview() {
     const [date, setDate] = React.useState<Date>()
@@ -14,18 +14,38 @@ export default function DeliveriesOverview() {
         from: startOfMonth(new Date()).toISOString(),
         to: endOfMonth(new Date()).toISOString()
     })
-
+    const [deliveryDays, setDeliveryDays] = useState<Matcher[] | undefined>()
+    const [deliveryCommands, setDeliveryCommands] = useState<CommandsEntity[] | undefined>()
     const {commands} = useGetAllCommands({
         filters: {
             from: month.from, 
             to: month.to
         }
     })
+    useEffect(()=>{
+        if(commands && commands.length){
+            const dates = commands.map((date)=> new Date(date.withdrawDate))
+            setDeliveryDays(dates)
+        }else{
+            setDeliveryCommands(undefined)
+        }
+    }, [commands])
+
     const handleMonthChange = (e: Date) => {
         setMonth({
             from: e.toISOString(), 
             to: endOfMonth(e).toISOString()
         })
+    }
+
+    const handleDayClick: DayMouseEventHandler = (day, {booked}) => {
+        if(booked){
+            setDeliveryCommands(commands?.filter((command) => {
+                return isSameDay(new Date(command.withdrawDate), day)
+            }))
+        }else{
+            setDeliveryCommands(undefined)
+        }
     }
 
   return (
@@ -36,14 +56,21 @@ export default function DeliveriesOverview() {
         <Calendar
             className="p-0"
             locale={fr}
+            modifiers={{booked: deliveryDays as Matcher[]}}
+            modifiersClassNames={{booked: "delivery-date"}}
             mode="single"
             onMonthChange={handleMonthChange}
             selected={date}
             onSelect={setDate}
-            components={{
-                DayContent: (props: DayContentProps) => <CalendarCustomDayContent props={props} commands={commands}/>
-            }}
+            onDayClick={handleDayClick}
         />
+        <div className="mt-8">
+            <div className="flex w-full flex-col gap-3">
+                {deliveryCommands && deliveryCommands.map((command) => (
+                    <CommandDeliveryListItem key={command.code} command={command}/>
+                ))}
+            </div>
+        </div>
     </div>
   )
 }
