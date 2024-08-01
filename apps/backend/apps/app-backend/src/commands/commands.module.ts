171581d2067f8895e85642308a39b012eebe15a9
NZIMA Ivan <nzimaivan007@gmail.com>
2024-08-01 18:39:32 +0100

    creating incomes microservice and link with app backend

--- apps/backend/apps/app-backend/src/commands/commands.module.ts
+++ apps/backend/apps/app-backend/src/commands/commands.module.ts
@@ -3,8 +3,30 @@ import { CommandsService } from './commands.service';
 import { CommandsController } from './commands.controller';
 import { PrismaService } from '@app-backend/prisma.service';
 import { InvoicesService } from '@app-backend/invoices/invoices.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    ClientsModule.registerAsync([
+      {
+        imports: [ConfigModule],
+        name: 'INCOMES_STATS',
+        useFactory: (configService: ConfigService) => {
+          return {
+            transport: Transport.RMQ,
+            options: {
+              urls: [configService.get<string>('RMQ_URI')],
+              queue: configService.get<string>('RMQ_INCOMES_STATS_QUEUE'),
+              noAck: false,
+              persistent: true,
+            },
+          };
+        },
+        inject: [ConfigService],
+      },
+    ]),
+  ],
   controllers: [CommandsController],
   providers: [CommandsService, PrismaService, InvoicesService],
 })
--- apps/backend/apps/incomes-stats/src/main.ts
+++ apps/backend/apps/incomes-stats/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { IncomesStatsModule } from './incomes-stats.module';
+import { RmqOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(IncomesStatsModule);
-  await app.listen(3000);
+  const configService = app.get<ConfigService>(ConfigService);
+
+  app.connectMicroservice<RmqOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [configService.get<string>('RMQ_URI')],
+      queue: configService.get<string>('RMQ_INCOMES_STATS_QUEUE'),
+      noAck: false,
+      persistent: true,
+    },
+  });
+  await app.startAllMicroservices();
 }
 bootstrap();
