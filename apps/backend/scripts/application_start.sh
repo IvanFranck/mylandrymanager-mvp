522d3d0fd85d8a12df1ca3005f6a51b45f154a0e
NZIMA Ivan <nzimaivan007@gmail.com>
2024-12-20 15:27:27 +0100

    wip, set up deployment pipeline

--- apps/backend/scripts/application_start.sh
+++ apps/backend/scripts/application_start.sh
@@ -4,5 +4,11 @@ echo 'run application_start.sh: ' >> /home/ec2-user/mlm-backend/deploy.log
 echo 'docker-compose up -d' >> /home/ec2-user/mlm-backend/deploy.log
 docker-compose up -d
 
-echo 'pnpm start:dev' >> /home/ec2-user/mlm-backend/deploy.log
-pnpm start:dev
+echo 'npx prisma migrate deploy' >> /home/ec2-user/mlm-backend/deploy.log
+npx prisma migrate deploy
+
+echo 'npx prisma generate' >> /home/ec2-user/mlm-backend/deploy.log
+npx prisma generate
+
+echo 'npm run start:dev' >> /home/ec2-user/mlm-backend/deploy.log
+npm run start:dev
