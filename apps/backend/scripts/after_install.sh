da7686e90256fb4079a965e21c68111a8bb049b6
NZIMA Ivan <nzimaivan007@gmail.com>
2024-12-21 12:19:06 +0100

    wip, set up deployment pipeline

--- apps/backend/scripts/after_install.sh
+++ apps/backend/scripts/after_install.sh
@@ -4,7 +4,7 @@ echo 'run after_install.sh: ' >> /home/ec2-user/mlm-backend/deploy.log
 echo 'cd /home/ec2-user/mlm-backend' >> /home/ec2-user/mlm-backend/deploy.log
 cd /home/ec2-user/mlm-backend >> /home/ec2-user/mlm-backend/deploy.log
 
-echo 'npm install' >> /home/ec2-user/mlm-backend/deploy.log 
+echo 'pnpm install' >> /home/ec2-user/mlm-backend/deploy.log 
 pnpm install >> /home/ec2-user/mlm-backend/deploy.log
 
 echo 'openssl genrsa -out ./private-key.pem 4096' >> /home/ec2-user/mlm-backend/deploy.log
