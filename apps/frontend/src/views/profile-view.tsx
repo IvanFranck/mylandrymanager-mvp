4d97e7b7d96147a3e7744cb56e64b23dd9ae8ec9
NZIMA Ivan <nzimaivan007@gmail.com>
2025-02-27 13:35:21 +0100

    feat: add loading skeleton for user profile view

--- apps/frontend/src/views/profile-view.tsx
+++ apps/frontend/src/views/profile-view.tsx
@@ -1,36 +1,45 @@
 import { useGetUserProfileInfos } from "@/lib/hooks/use-cases/profile/useGetUserProfileInfos"
 import { MapPin, Phone } from "lucide-react"
+import UserProfileSkeleton from '../components/app/profile/user-profile-skeleton';
 
 export function ProfileView() {
     const {userProfile, isFetching} = useGetUserProfileInfos()
 
     return (
         <div className="px-3">
-            <div className="w-full text-center mt-8">
-                <p className="text-base font-thin text-gray-400">Nom du pressing</p>
-                <h1 className="text-2xl font-semibold">{userProfile?.username}</h1>
+        {
+            isFetching ? <UserProfileSkeleton/> : 
+            (
+                <>
+                    <div className="w-full text-center mt-8">
+                        <p className="text-base font-thin text-gray-400">Nom du pressing</p>
+                        <h1 className="text-2xl font-semibold">{userProfile?.username}</h1>
 
-                <div className="mt-4">
-                    <button className="py-2 px-6 bg-purple-700 text-white tracking-wider  rounded-2xl font-light">Modifier</button>
-                </div>
-            </div>
+                        <div className="mt-4">
+                            <button className="py-2 px-6 bg-purple-700 text-white tracking-wider  rounded-2xl font-light">Modifier</button>
+                        </div>
+                    </div>
 
-            <div className="w-full mt-8 flex flex-col gap-8 bg-white rounded-2xl p-4 shadow-lg">
-                <div className="flex flex-col">
-                    <p className="text-base font-thin text-gray-500 flex justify-center items-center w-max">
-                        <MapPin className="mr-2 w-4 h-5" /> 
-                        <span>Adresse</span>
-                    </p>
-                    <p className="text-lg font-semibold">{userProfile?.address}</p>
-                </div>
-                <div className="flex flex-col">
-                    <p className="text-base font-thin text-gray-500 flex justify-center items-center w-max">
-                        <Phone className="mr-2 w-4 h-4" /> 
-                        <span>Téléphone</span>
-                    </p>
-                    <p className="text-lg font-semibold">{userProfile?.phone}</p>
-                </div>
-            </div>
+                    <div className="w-full mt-8 flex flex-col gap-8 bg-white rounded-2xl p-4 shadow-lg">
+                        <div className="flex flex-col">
+                            <p className="text-base font-thin text-gray-500 flex justify-center items-center w-max">
+                                <MapPin className="mr-2 w-4 h-5" /> 
+                                <span>Adresse</span>
+                            </p>
+                            <p className="text-lg font-semibold">{userProfile?.address}</p>
+                        </div>
+                        <div className="flex flex-col">
+                            <p className="text-base font-thin text-gray-500 flex justify-center items-center w-max">
+                                <Phone className="mr-2 w-4 h-4" /> 
+                                <span>Téléphone</span>
+                            </p>
+                            <p className="text-lg font-semibold">{userProfile?.phone}</p>
+                        </div>
+                    </div>
+                </>
+            )
+        }
         </div>
+        
     )
 }
