ef5797571ed725374c06b4269bf057a04b5b4768
NZIMA Ivan <nzimaivan007@gmail.com>
2025-02-27 14:05:41 +0100

    test pipeline

--- apps/frontend/src/views/login-view.tsx
+++ apps/frontend/src/views/login-view.tsx
@@ -17,7 +17,7 @@ export default function LoginView() {
     return (
         <div className="w-full min-h-screen flex flex-col justify-center items-center bg-black text-white">
             <div className="flex flex-col w-full px-5">
-                <h1 className="text-4xl font-medium">Connectez vous à votre compte.</h1>
+                <h1 className="text-4xl font-medium">Connectez vous à votre compte!</h1>
                 <div className="mt-8">
                     <GenericForm
                         schema={LoginFormSchema}
